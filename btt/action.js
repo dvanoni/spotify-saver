b13d153116f0fbb48be83902814e1cb34b271d61
David Vanoni <david@dvanoni.com>
2020-03-17 00:27:55 -0700

    Add trigger/action scripts for use in BetterTouchTool

---
+++ btt/action.js
@@ -0,0 +1,17 @@
+(async () => {
+  const npmPath = '$HOME/.nodenv/shims/npm'
+  const projectPath = '$HOME/Developer/projects/spotify-saver'
+
+  const isSpotifyPlaying = await callBTT(
+    'get_string_variable',
+    { variable_name: 'BTTCurrentlyPlayingApp' }
+  ) === 'com.spotify.client'
+
+  if (!isSpotifyPlaying) return
+
+  const result = await runShellScript({
+    script: `${npmPath} --prefix ${projectPath} run toggle-save-track`
+  })
+
+  returnToBTT(result)
+})()
---
+++ btt/trigger.js
@@ -0,0 +1,28 @@
+(async () => {
+  const npmPath = '$HOME/.nodenv/shims/npm'
+  const projectPath = '$HOME/Developer/projects/spotify-saver'
+  const iconPath = '/Applications/Setapp/BetterTouchTool.app/Contents/Resources/icons/ioniconpng'
+
+  const isSpotifyPlaying = await callBTT(
+    'get_string_variable',
+    { variable_name: 'BTTCurrentlyPlayingApp' }
+  ) === 'com.spotify.client'
+
+  if (!isSpotifyPlaying) {
+    return returnToBTT(JSON.stringify({ text: '' }))
+  }
+
+  const isTrackSaved = JSON.parse(await runShellScript({
+    script: `${npmPath} --prefix ${projectPath} run is-track-saved`
+  }))
+
+  const result = {
+    text: isTrackSaved ? 'Saved' : 'Not Saved',
+    font_color: isTrackSaved ? '88,182,96,255' : '255,255,255,255',
+    icon_path: isTrackSaved
+      ? `${iconPath}/md-heart.png`
+      : `${iconPath}/md-heart-empty.png`,
+  }
+
+  returnToBTT(JSON.stringify(result))
+})()
